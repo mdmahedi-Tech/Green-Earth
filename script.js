6b9d72cbff3afb00e79ee1b7a7a84ef8cc946fbd
mdmahediTech <mehedihasan51020510@gmail.com>
2026-04-01 02:10:52 -0700

    cart part is developed

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const catchcontainer=document.getElementById('catagery-container');
 const treecontainer=document.getElementById('treeContainer');
 const loadingSpeener=document.getElementById('loadingspeener');
 const alltreesbutton=document.getElementById('alltreesbutton');
+const cartContainer= document.getElementById('cartContainer');
+
+const cart=[];
 
         //    loadcategpry button
  async function loadCategory(){
@@ -105,7 +108,7 @@ function displaytrees(trees){
             <div class="badge badge-warning">${tree.category}</div>
             <div class="flex justify-between">
                 <h2 class="font-bold text-3xl">${tree.price}</h2>
-            <button class="btn btn-primary">add card</button>
+            <button class="btn btn-primary" onclick="addToCart(${tree.id},'${tree.name}',${tree.price})">add card</button>
             </div>
         </div>
         </div>
@@ -113,6 +116,55 @@ function displaytrees(trees){
     `
     treecontainer.appendChild(card)
     })
+}
+
+// add to cart
+function addToCart(id,name,price){
+    console.log(id,name,price)
+    const existingcard=cart.find((item)=>item.id===id)
+    if(existingcard){
+        existingcard.quantity+=1;
+    }
+    else{
+           cart.push({
+        id,
+        name,
+        price,
+        quantity:1,
+    });
+    }
+   
+    updateCart()
+}
+
+function updateCart(){
+cartContainer.innerHTML="";
+console.log(cart);
+cart.forEach(item => {
+    const cartitem=document.createElement('div')
+    cartitem.className="space-y-2";
+    cartitem.innerHTML=`
+        
+            <div class="bg-cyan-200">
+             <div class="flex justify-between items-center">
+                   <div>
+                   <h2 class="font-bold">${item.name}</h2>
+                  <p class="font-bold">${item.price}x${item.quantity}</p>
+                  </div>
+                
+                   <button class="font-bold">x</button>
+               </div>
+              
+                  <p class="text-right font-semibold">$${item.price *  item.quantity}</p>
+            </div>
+           
+             
+
+
+    `
+    cartContainer.appendChild(cartitem)
+
+});
 }
  loadCategory()
 loadtrees()
